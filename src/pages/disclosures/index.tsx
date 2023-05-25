c49f115f3d1aaf64ec33943f6c4eaa8ff84263a5
Matthew Galligan <matthew.galligan@cisa.dhs.gov>
2023-05-25 13:24:52 -0400

    Remove autth showcases

--- src/pages/disclosures/index.tsx
+++ src/pages/disclosures/index.tsx
@@ -192,20 +192,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-    </div>
-  );
-};
