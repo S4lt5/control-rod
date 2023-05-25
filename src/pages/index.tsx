ac85e276d345183d8fd5a37aa636d22e2402b8fc
Matthew Galligan <matthew.galligan@cisa.dhs.gov>
2023-05-25 12:33:20 -0400

    Add "no findings" alert just like on disclosure page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -179,33 +179,18 @@ const Home: NextPage = () => {
             Loading...
           </button>
         )}
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+        {findingsStatus &&
+          findingsStatus != 'loading' &&
+          findings &&
+          findings.length == 0 && (
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex flex-col items-center justify-center gap-4">
+                <p className="text-center text-2xl text-white">
+                  No Findings found, have you run any scans?
+                </p>
+              </div>
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <AuthShowcase />
-        </div>
+          )}
       </div>
     </>
   );
