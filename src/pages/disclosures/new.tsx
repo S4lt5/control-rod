1d1f5e3933e0fe70fca76f941f4596ff2b95aca4
Matthew Galligan <matthew.galligan@cisa.dhs.gov>
2023-07-05 11:28:42 -0400

    Add "loading" to new disclosure page

--- src/pages/disclosures/new.tsx
+++ src/pages/disclosures/new.tsx
@@ -5,8 +5,11 @@ import { type SyntheticEvent, useState } from 'react';
 import { createCompareFn } from '~/shared/helpers';
 import { api } from '~/utils/api';
 import { useRouter } from 'next/router';
-import { matchesMiddleware } from 'next/dist/shared/lib/router/router';
-import { Disclosure, Finding, disclosureStatus } from '@prisma/client';
+import {
+  type Disclosure,
+  type Finding,
+  disclosureStatus,
+} from '@prisma/client';
 import { createFindingFilterFn } from '~/shared/finding';
 
 const NewDisclosure: NextPage = () => {
@@ -142,6 +145,37 @@ const NewDisclosure: NextPage = () => {
                     </tbody>
                   </table>
                 </div>
+                {((findingsStatus && findingsStatus == 'loading') ||
+                  (disclosureQueryStatus &&
+                    disclosureQueryStatus == 'loading')) && (
+                  <button
+                    type="button"
+                    className="inline-flex cursor-not-allowed items-center rounded-md  bg-indigo-400 px-4 py-2 text-sm font-semibold leading-6 text-white shadow transition duration-150 ease-in-out"
+                    disabled
+                  >
+                    <svg
+                      className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth={4}
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    Loading...
+                  </button>
+                )}
                 <div
                   className="search-icon absolute"
                   style={{ top: '0.1rem', left: '.8rem' }}
